560e0167c18267a415b1739760a4b457f5a76318
Anslem Coelho <anslem1999@gmail.com>
2025-05-30 10:55:39 -0400

    Summary in q4 and new q5

--- Assignment 1/Controllers/q4Controller.cs
+++ Assignment 1/Controllers/q4Controller.cs
@@ -7,8 +7,18 @@ namespace Assignment_1.Controllers
     [ApiController]
     public class q4Controller : ControllerBase
     {
-        [HttpPost("knockknock")]
-        public string Get()
+        /// <summary>
+        /// It begins responding to the knock knock joke
+        /// </summary>
+        /// <returns>
+        /// response to the first line of the knockknock joke
+        /// </returns>
+        /// <example>
+        ///  api/q4/knockknock -> "Who's there?"
+        ///  Request Body: None
+        /// </example>
+        [HttpPost(template: "knockknock")]
+        public string Knockknock()
         {
             return "Who's there?";
         }
---
+++ Assignment 1/Controllers/q5Controller.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class q5Controller : ControllerBase
+    {
+        /// <summary>
+        /// This code uses a value from input and uses it to return a statement
+        /// </summary>
+        /// <param name="code">The secret code that the user will input</param>
+        /// <returns>
+        /// A statement that uses the input and calls it a secret
+        /// </returns>
+        /// <example>
+        /// https://localhost:XX/api/q5/Secret -> "Shh.. the secret is 90"
+        /// Request Body: "90"
+        /// Content-Type: application/json
+        /// </example>
+        [HttpPost(template: "secret")]
+
+        public string Secret([FromBody] string code)
+        {
+            string Message = "Shh..the secret is " + code.ToString();
+            return Message;
+        }
+    }
+}
